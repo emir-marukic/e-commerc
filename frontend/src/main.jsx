fb4726afcb7c089dbf685338841c7e423403a8b1
emir-marukic <marukicemir33@hotmail.com>
2023-12-19 00:26:01 +0100

    cmt

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import Test from './Test'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Test />
   </React.StrictMode>,
 )
