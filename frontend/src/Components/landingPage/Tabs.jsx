902b6ca8d308fc9a28a4d06b45d7d802f5138198
emir-marukic <marukicemir33@hotmail.com>
2023-12-31 14:09:22 +0100

    cmt

--- frontend/src/Components/landingPage/Tabs.jsx
+++ frontend/src/Components/landingPage/Tabs.jsx
@@ -8,6 +8,8 @@ import { useState } from "react";
 import MediaCard from "./Card";
 import { useEffect } from "react";
 import { devicesApi } from "../../api/App";
+import "../../styles/Navbar.css";
+import { Grid } from "@mui/material";
 // import { devicesApi } from "../../api/App";
 
 function CustomTabPanel(props) {
@@ -22,7 +24,7 @@ function CustomTabPanel(props) {
       {...other}
     >
       {value === index && (
-        <Box sx={{ p: 3 }}>
+        <Box sx={{ p: 3 }} className="card">
           <Typography>{children}</Typography>
         </Box>
       )}
@@ -47,6 +49,8 @@ export default function BasicTabs() {
   const [value, setValue] = React.useState(0);
   const [samsung, setSamsung] = useState(null);
   const [iphone, setIphone] = useState(null);
+  const [samsungWatch, setSamsungWatch] = useState(null);
+  const [iphoneWatch, setIphoneWatch] = useState(null);
 
   useEffect(() => {
     const fetchData = async (str) => {
@@ -56,13 +60,16 @@ export default function BasicTabs() {
           setSamsung(response.data.data);
         } else if (str === "phones") {
           setIphone(response.data.data);
+        } else if (str === "samsung-smart-watches") {
+          setSamsungWatch(response.data.data);
+        } else if (str === "iphone-watches") {
+          setIphoneWatch(response.data.data);
         }
       } catch (error) {
         console.error("Error fetching data: ", error);
       }
     };
 
-    // Fetch data for the initially selected tab
     fetchData(getTabDataName(value));
   }, [value]);
 
@@ -71,18 +78,16 @@ export default function BasicTabs() {
   };
 
   const getTabDataName = (tabIndex) => {
-    // Define a mapping between tab index and data names
     const tabDataNames = [
       "samsungs",
-      "iphones",
-      "samsungWatches",
-      "iphoneWatches",
+      "phones",
+      "samsung-smart-watches",
+      "iphone-watches",
     ];
     return tabDataNames[tabIndex];
   };
 
   const handleTabChange = (event, newValue) => {
-    // Fetch data when a tab is changed
     const dataName = getTabDataName(newValue);
     fetchData(dataName);
     handleChange(event, newValue);
@@ -98,11 +103,11 @@ export default function BasicTabs() {
   };
 
   return (
-    <Box sx={{ width: "100%" }}>
-      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+    <Box sx={{ width: "100%", mx: "auto" }}>
+      <Box sx={{ borderBottom: 1, borderColor: "divider" }} className="tabs">
         <Tabs
           value={value}
-          onChange={handleTabChange} // Use handleTabChange for tab changes
+          onChange={handleTabChange}
           aria-label="basic tabs example"
         >
           <Tab label="Samsung" {...a11yProps(0)} />
@@ -111,9 +116,24 @@ export default function BasicTabs() {
           <Tab label="iPhone Smart Watches" {...a11yProps(3)} />
         </Tabs>
       </Box>
-      <CustomTabPanel value={value} index={0}>
-        {samsung &&
-          samsung.map((item) => (
+      <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+        <Grid item xs={6}>
+          <CustomTabPanel value={value} index={0}>
+            {samsung &&
+              samsung.map((item) => (
+                <MediaCard
+                  key={item.id}
+                  name={item.attributes.model}
+                  img={item.attributes.img}
+                  price={item.attributes.price}
+                />
+              ))}
+          </CustomTabPanel>
+        </Grid>
+      </Grid>
+      <CustomTabPanel value={value} index={1}>
+        {iphone &&
+          iphone.map((item) => (
             <MediaCard
               key={item.id}
               name={item.attributes.model}
@@ -122,9 +142,20 @@ export default function BasicTabs() {
             />
           ))}
       </CustomTabPanel>
-      <CustomTabPanel value={value} index={0}>
-        {iphone &&
-          iphone.map((item) => (
+      <CustomTabPanel value={value} index={2}>
+        {samsungWatch &&
+          samsungWatch.map((item) => (
+            <MediaCard
+              key={item.id}
+              name={item.attributes.model}
+              img={item.attributes.img}
+              price={item.attributes.price}
+            />
+          ))}
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={3}>
+        {iphoneWatch &&
+          iphoneWatch.map((item) => (
             <MediaCard
               key={item.id}
               name={item.attributes.model}
